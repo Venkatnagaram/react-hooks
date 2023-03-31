3fa0bb26fe51e5d78626f72ac19eb9ef1db4d58e
Venkat Nagaram <nagaram.venkat@gmail.com>
2023-03-31 19:06:00 +0530

    useEffect
    useEffect

--- src/components/useEffect/useEffect.js
+++ src/components/useEffect/useEffect.js
@@ -1,16 +1,27 @@
 import React,{useEffect, useState} from "react";
 const UseEffect = () => {
-    const [count,setCount]=useState(0);
+    const [users,setUsers]=useState([]);
     useEffect(()=>{
-        setTimeout(()=>{
-            setCount(count + 1)
-        }, 5000)
-    },[count])
+        getApiData();
+    },[])
+    // Function to collect data
+    const getApiData = async () => {
+        const data = await fetch(
+        "https://jsonplaceholder.typicode.com/users/"
+        ).then((response) => response.json());
+    
+        // update the state
+        setUsers(data);
+    };
     return(
         <>
             <h2>useEffect</h2>
             <p>useEffect is trigger after return statment. useEffect hook allow you perform side effects in function components. fetch data is one example of side effect.</p>
-            <p>Count will update in every 5 seconds: {count}</p>
+            <ul>
+                {
+                    users.map(user=><li key={user.id}><b>{user.id}.</b> {user.name}</li>)
+                }
+            </ul>
         </>
     )
 }
